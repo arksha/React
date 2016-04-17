9353be03507ab51547828b8255c9331dda46b784
arksha <tingg@usc.edu>
2016-04-17 00:58:04 -0700

    add multiple child element to show many notes

--- js/Note.js
+++ js/Note.js
@@ -56,8 +56,25 @@ var Board = React.createClass({
 			}
 		}	
 	},
+	getInitialState: function(){
+		return {
+			notes:[
+				"call mom",
+				"go swimming",
+				"get mail",
+				"finish HW4"
+			]
+	
+		};
+	},
 	render: function(){
-		return <div className="board">{this.props.count}</div>
+		return (<div className="board">
+				{this.state.notes.map(function(note,i){
+					return(
+						<Note key={i}>{note}</Note>
+					);
+				})}
+				</div>);
 	}
 });
 React.render(<Board count={10}/>, 
