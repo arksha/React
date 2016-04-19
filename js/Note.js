ce40d162d0625c24e36c1ecd0ef37770dd2f66e7
arksha <tingg@usc.edu>
2016-04-19 13:01:53 -0700

    add random position with note
    1.thoughts of key props, when use dynamic key
    2. randomBetween: generate new random num
    3.componentWillMount: hold position to a new style
    4.nextID: generate new id for note

--- js/Note.js
+++ js/Note.js
@@ -2,11 +2,26 @@ var Note = React.createClass({
     getInitialState: function() {
         return {editing: false}
     },
+	componentWillMount: function() {
+		//fire right before the first random
+		//take care of position of random notes
+		//150 is note width
+        this.style = {//new object
+            right: this.randomBetween(0, window.innerWidth - 150) + 'px',
+            top: this.randomBetween(0, window.innerHeight - 150) + 'px',
+            transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
+        };
+    },
+    randomBetween: function(min, max) {
+		//create a random number between 2 numbers to get random position of notes
+        return (min + Math.ceil(Math.random() * max));
+    },
     edit: function() {
         this.setState({editing: true});
     },
     save: function() {
-		this.props.onChange(this.refs.newvalue.getDOMNode().value,this.props.index);//invoke onChange to get update function invoke
+		this.props.onChange(this.refs.newvalue.getDOMNode().value,this.props.index);
+		//invoke onChange to get update function invoke
         this.setState({editing: false});
     },
     remove: function() {
@@ -14,7 +29,7 @@ var Note = React.createClass({
     },
     renderDisplay: function() {
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
                 <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.edit}
@@ -27,10 +42,10 @@ var Note = React.createClass({
     },
     renderForm: function() {
         return (
-            <div className="note">
-            <textarea ref="newvalue" defaultValue={this.props.children} 
-            className="form-control"></textarea>
-            <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+            <div className="note" style={this.style}>
+				<textarea ref="newvalue" defaultValue={this.props.children} 
+				className="form-control"></textarea>
+				<button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
             </div>
             )
     },
@@ -60,9 +75,16 @@ var Board = React.createClass({
 			notes:[]
 		};
 	},
+	nextID: function(){
+		this.uniqueId = this.uniqueId || 0;
+		return this.uniqueId ++;
+	},
 	add: function(text){
 		var arr = this.state.notes;
-		arr.push(text)//use push to get added text into notes
+		arr.push({
+			id: this.nextID(),
+			note: text
+		});//use push to get added text into notes
 		this.setState({note:arr});//what is the parameter in setState?
 	},
 	update: function(newvalue,i){
@@ -79,11 +101,11 @@ var Board = React.createClass({
 	eachnote:function(note,i){
 		//is going to return each note instead of put it in render funciton
 		return(
-			<Note key={i}
+			<Note key={note.id}
 				index={i}
 				onChange={this.update}
 				onRemove={this.remove}
-			>{note}</Note>
+			>{note.note}</Note>
 		);
 	},
 	render: function(){
